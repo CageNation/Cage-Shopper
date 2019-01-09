2aabb3586265a4b418f303eff8da50ab0b094054
Robin <robinleesuns13@gmail.com>
2019-01-09 14:08:46 -0800

    Add public key

--- client/components/Checkout/constants/stripe.js
+++ client/components/Checkout/constants/stripe.js
@@ -1,4 +1,6 @@
 const STRIPE_PUBLISHABLE =
-  process.env.NODE_ENV === 'production' ? 'pk_test' : 'pk_test'
+  process.env.NODE_ENV === 'production'
+    ? 'pk_test_O9IIzZ75ZmAq1cxyUrFhMMNM'
+    : 'pk_test_O9IIzZ75ZmAq1cxyUrFhMMNM'
 
 export default STRIPE_PUBLISHABLE
