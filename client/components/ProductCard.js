070e7f161f9b0aa950f2529ee5f9096067c60874
vasquezjr <Jose0x52Vasquez@gmail.com>
2019-01-13 13:06:11 -0800

    Feat: Simple Styling for Product Card

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -3,6 +3,8 @@ import {clearCart, setCart} from '../store'
 import {connect} from 'react-redux'
 import axios from 'axios'
 
+import {Button, Icon, Image, Item, Label} from 'semantic-ui-react'
+
 const addToCart = async (product, setCartSize, user) => {
   const currentItems = JSON.parse(localStorage.getItem('cart'))
 
@@ -23,19 +25,47 @@ const addToCart = async (product, setCartSize, user) => {
 
 const ProductCard = props => {
   const {product, setCartSize, user} = props
+  // return (
+  //   <div className="productCard">
+  //     <p>{`product name: ${product.name}`}</p>
+  //     <img src={product.imageUrl} />
+  //     <p>{`description: ${product.description}`}</p>
+  //     <h3>{`price: ${(product.price / 100).toFixed(2)}`}</h3>
+  //     <button
+  //       type="submit"
+  //       onClick={() => addToCart(product, setCartSize, user)}
+  //     >
+  //       ADD TO CART
+  //     </button>
+  //   </div>
+  // )
+
   return (
-    <div className="productCard">
-      <p>{`product name: ${product.name}`}</p>
-      <img src={product.imageUrl} />
-      <p>{`description: ${product.description}`}</p>
-      <h3>{`price: ${(product.price / 100).toFixed(2)}`}</h3>
-      <button
-        type="submit"
-        onClick={() => addToCart(product, setCartSize, user)}
-      >
-        ADD TO CART
-      </button>
-    </div>
+    <Item.Group divided>
+      <Item>
+        <Item.Image src={product.imageUrl} />
+
+        <Item.Content>
+          <Item.Header as="a">{product.name}</Item.Header>
+          <Item.Meta>
+            <span className="cinema">Description:</span>
+          </Item.Meta>
+          <Item.Description>{product.description}</Item.Description>
+          <Item.Extra>
+            <Button
+              onClick={() => addToCart(product, setCartSize, user)}
+              primary
+              floated="right"
+            >
+              ADD TO CART
+              <Icon name="right chevron" />
+            </Button>
+            <Label>Details</Label>
+            <Label>Details</Label>
+          </Item.Extra>
+        </Item.Content>
+      </Item>
+    </Item.Group>
   )
 }
 
