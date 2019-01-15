b6651a331b654c5edc5c1e724c73ce58ad31bc02
vasquezjr <Jose0x52Vasquez@gmail.com>
2019-01-15 05:25:03 -0800

    Feat: Added Accordian to Orders in Profile Page

--- client/components/Profile.js
+++ client/components/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Table} from 'semantic-ui-react'
+import {Accordion, Table, Menu} from 'semantic-ui-react'
 import axios from 'axios'
 import ReactHtmlParser from 'react-html-parser'
 
@@ -16,72 +16,40 @@ class Profile extends React.Component {
 
   async componentDidMount() {
     const user = await axios.get(`/api/users/${this.props.user.id}`)
-    this.setState(...this.state.orders, {orders: user.data.orders})
+    this.setState({orders: user.data.orders})
   }
 
   render() {
-    const orders = this.state.orders
+    const {orders} = this.state
     let totalPrice = 0
     return (
       <div>
         <h1>Welcome, {this.props.user.email}</h1>
-        <h4>
-          This is your personal account page. Too bad there's nothing for you to
-          do here :(
-        </h4>
+        <h4>This is your personal account page.</h4>
 
         {orders ? (
           <div>
             <h2>Order History</h2>
-            {orders.map((order, idx) => {
+            {orders.reverse().map((order, idx) => {
               this.totalPrice = 0
               const products = JSON.parse(orders[idx].orderData)
+              const orderNumber = `Order #${1000 + order.id}`
 
               if (order.completed) {
                 return (
-                  <Table size="large" key={order.id}>
-                    <Table.Header>
-                      <Table.Row>
-                        <Table.HeaderCell>
-                          Order #{1000 + order.id}
-                        </Table.HeaderCell>
-                      </Table.Row>
-                      <Table.Row>
-                        <Table.HeaderCell>Name</Table.HeaderCell>
-                        <Table.HeaderCell>Description</Table.HeaderCell>
-                        <Table.HeaderCell>Price</Table.HeaderCell>
-                        <Table.HeaderCell />
-                      </Table.Row>
-                    </Table.Header>
-                    <Table.Body>
-                      {products.map((product, idx) => {
-                        totalPrice += product.price
-                        return (
-                          <Table.Row key={idx + product.name}>
-                            <Table.Cell>{product.name}</Table.Cell>
-                            <Table.Cell>
-                              {ReactHtmlParser(product.description)}
-                            </Table.Cell>
-                            <Table.Cell>
-                              {(product.price / 100).toFixed(2)}
-                            </Table.Cell>
-                          </Table.Row>
-                        )
-                      })}
-                    </Table.Body>
-                    <Table.Footer>
-                      <Table.Row>
-                        <Table.HeaderCell>
-                          <b>Total Price</b>
-                        </Table.HeaderCell>
-                        <Table.HeaderCell />
-                        <Table.HeaderCell>
-                          {(totalPrice / 100).toFixed(2)}
-                        </Table.HeaderCell>
-                        <Table.HeaderCell />
-                      </Table.Row>
-                    </Table.Footer>
-                  </Table>
+                  <React.Fragment key={orderNumber}>
+                    <Accordion
+                      panels={[
+                        {
+                          key: orderNumber,
+                          title: orderNumber,
+                          content: {
+                            content: orderTable(order.id, products, totalPrice)
+                          }
+                        }
+                      ]}
+                    />
+                  </React.Fragment>
                 )
               } else {
                 return <div />
@@ -94,6 +62,48 @@ class Profile extends React.Component {
       </div>
     )
   }
+}
+
+const orderTable = (id, products, totalPrice) => {
+  return (
+    <Table size="large" key={id}>
+      <Table.Header>
+        {/* <Table.Row>
+          <Table.HeaderCell>
+            Order #{1000 + id}
+          </Table.HeaderCell>
+        </Table.Row> */}
+        <Table.Row>
+          <Table.HeaderCell>Name</Table.HeaderCell>
+          <Table.HeaderCell>Description</Table.HeaderCell>
+          <Table.HeaderCell>Price</Table.HeaderCell>
+          <Table.HeaderCell />
+        </Table.Row>
+      </Table.Header>
+      <Table.Body>
+        {products.map((product, idx) => {
+          totalPrice += product.price
+          return (
+            <Table.Row key={idx + product.name}>
+              <Table.Cell>{product.name}</Table.Cell>
+              <Table.Cell>{ReactHtmlParser(product.description)}</Table.Cell>
+              <Table.Cell>{(product.price / 100).toFixed(2)}</Table.Cell>
+            </Table.Row>
+          )
+        })}
+      </Table.Body>
+      <Table.Footer>
+        <Table.Row>
+          <Table.HeaderCell>
+            <b>Total Price</b>
+          </Table.HeaderCell>
+          <Table.HeaderCell />
+          <Table.HeaderCell>{(totalPrice / 100).toFixed(2)}</Table.HeaderCell>
+          <Table.HeaderCell />
+        </Table.Row>
+      </Table.Footer>
+    </Table>
+  )
 }
 
 /**
